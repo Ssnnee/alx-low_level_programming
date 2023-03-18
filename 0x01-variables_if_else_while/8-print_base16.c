f39b6b9d8c849b106efb07dd6d2b9df044af08ed
Samuel Nandi <103636557+Ssnnee@users.noreply.github.com>
2023-03-18 01:41:21 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,13 +9,13 @@ int main(void)
 {
     int i;
     char hex[] = "0123456789abcdef";
-
+    
     for (i = 0; i < 16; i++)
     {
         putchar(hex[i]);
     }
     putchar('\n');
-
-    return 0;
+    
+    return (0);
 }
 
