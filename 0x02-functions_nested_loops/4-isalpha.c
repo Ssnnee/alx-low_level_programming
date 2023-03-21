4bf026ca7e2a444aff23a5a41fd6eb6e5d9f6dcf
Samuel Nandi <nandisamuelsne@gmail.com>
2023-03-21 16:15:42 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,6 +4,9 @@
  * _islower - Checks for alphebetic character by showing 1 if true otherwise 0.
  * @c: The character in ASCII code
  *
+ * Description: Determines whether a given character is an alphabetic character
+ * in the ASCII table by checking if it falls within the range of lowercase
+ * letters (97-122) or uppercase letters (65-90).
  * Return: Return 1 if true otherwise 0.
  */
 
