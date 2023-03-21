beae975cc22059b0aee2bfad69a5a4374acc619a
Samuel Nandi <nandisamuelsne@gmail.com>
2023-03-21 16:22:18 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,12 @@
 #include "main.h"
-
 /**
- * _islower - Checks for alphebetic character by showing 1 if true otherwise 0.
- * @c: The character in ASCII code
- *
- * Description: Determines whether a given character is an alphabetic character
- * in the ASCII table by checking if it falls within the range of lowercase
- * letters (97-122) or uppercase letters (65-90).
- * Return: Return 1 if true otherwise 0.
- */
-
+* _isalpha - Shows 1 if the input is c
+* letter Another cases, shows 0
+*
+* @c: The character in ASCII code
+*
+* Return: 1 for letters. Returns 0 otherwise
+*/
 int _isalpha(int c)
 {
 	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
