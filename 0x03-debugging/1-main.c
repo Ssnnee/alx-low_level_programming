d8b8cb34a2fd4e9a6c896b33b344de2ab6ca58cc
Samuel Nandi <nandisamuelsne@gmail.com>
2023-03-23 05:56:57 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	printf("Infinite loop incoming :(\n");
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+
+}
