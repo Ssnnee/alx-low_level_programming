237648cd5a16257b5c85b22b4c167a3a4a743981
Samuel Nandi <nandisamuelsne@gmail.com>
2023-03-23 16:34:04 -0700

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,6 @@ int main(void)
 	int i;
 
 	for (i = 1; i < 100; i++)
-	{
 		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
@@ -26,8 +25,7 @@ int main(void)
 		{
 			printf("%d ", i);
 		}
-		printf("Buzz");
-		printf("\n");
-		return (0);
-	}
+	printf("Buzz");
+	printf("\n");
+	return (0);
 }
