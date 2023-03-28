acdae4d5aadda52951354301d5057ae6fba19a79
Samuel Nandi <nandisamuelsne@gmail.com>
2023-03-27 18:07:47 -0700

    10 #advanced

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,16 +11,17 @@ int _atoi(char *s)
 	int num = 0;
 	int i = 0;
 
+	if (s == NULL || *s == '\0')
+		return 0;
+
 	while (s[i] != '\0')
 	{
 		if (s[i] == '-')
-			sign *= -1;
-		if (s[i] >= '0' && s[i] <= '9')
-		{
+			sign *= -sign;
+		else if (s[i] >= '0' && s[i] <= '9')
 			num = num * 10 + (s[i] - '0');
-			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
-				break;
-		}
+		else if (num > 0)
+			break;
 		i++;
 	}
 	return (num * sign);
