4949fba6105d7b9456c5588b6211d90739f6c37e
Samuel Nandi <nandisamuelsne@gmail.com>
2023-03-27 18:57:24 -0700

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,41 +9,47 @@
  */
 int main(void)
 {
-    char password[12];
-    int i;
+    char password[13]; /* increase array size to accommodate null terminator*/
 
-    srand(time(NULL)); /* seed the random number generator */
+    srand(time(NULL)); /* seed the random number generator*/
 
     /* generate first character (capital letter) */
     password[0] = rand() % 26 + 'A';
 
     /* generate next two characters (digits) */
-    for (i = 1; i <= 2; i++) {
+    for (int i = 1; i <= 2; i++) {
         password[i] = rand() % 10 + '0';
     }
 
-    /* generate next two characters (small letters) */
-    for (i = 3; i <= 4; i++) {
+    /* generate next two characters (small letters) */ 
+    for (int i = 3; i <= 4; i++) {
         password[i] = rand() % 26 + 'a';
     }
 
-    /* generate next two characters (other characters) */
-    for (i = 5; i <= 6; i++) {
-        password[i] = rand() % 15 + 33; /* ASCII values for special characters */
+    /* generate next two characters (special characters) */
+    for (int i = 5; i <= 6; i++) {
+        password[i] = rand() % 15 + 33; /* ASCII values for special characters */ 
     }
 
-    /* generate last four characters (alternating digits and small letters) */
-    for (i = 7; i <= 10; i++) {
-        if (i % 2 == 1) {
+    /* generate next two characters (uppercase letters) */
+    for (int i = 7; i <= 8; i++) {
+        password[i] = rand() % 26 + 'A';
+    }
+
+    /* generate last three characters (alternating digits, uppercase letters and small letters) */ 
+    for (int i = 9; i <= 11; i++) {
+        if (i % 3 == 0) {
             password[i] = rand() % 10 + '0';
-        } else {
+        } else if (i % 3 == 1) {
             password[i] = rand() % 26 + 'a';
+        } else {
+            password[i] = rand() % 26 + 'A';
         }
     }
 
-    password[11] = '\0'; /* add null terminator */
+    password[12] = '\0'; /* add null terminator */ 
 
-    printf("%s\n", password); /* print the generated password */
+    printf("%s\n", password); /* print the generated password */ 
 
     return 0;
 }
