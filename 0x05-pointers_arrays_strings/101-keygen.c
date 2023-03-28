835ef2a04904f2a00ddede04845ffb59d1a715b8
Samuel Nandi <nandisamuelsne@gmail.com>
2023-03-27 18:40:56 -0700

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,27 +12,27 @@ int main(void)
     char password[12];
     int i;
 
-    srand(time(NULL)); // seed the random number generator
+    srand(time(NULL)); /* seed the random number generator */
 
-    // generate first character (capital letter)
+    /* generate first character (capital letter) */
     password[0] = rand() % 26 + 'A';
 
-    // generate next two characters (digits)
+    /* generate next two characters (digits) */
     for (i = 1; i <= 2; i++) {
         password[i] = rand() % 10 + '0';
     }
 
-    // generate next two characters (small letters)
+    /* generate next two characters (small letters) */
     for (i = 3; i <= 4; i++) {
         password[i] = rand() % 26 + 'a';
     }
 
-    // generate next two characters (other characters)
+    /* generate next two characters (other characters) */
     for (i = 5; i <= 6; i++) {
-        password[i] = rand() % 15 + 33; // ASCII values for special characters
+        password[i] = rand() % 15 + 33; /* ASCII values for special characters */
     }
 
-    // generate last four characters (alternating digits and small letters)
+    /* generate last four characters (alternating digits and small letters) */
     for (i = 7; i <= 10; i++) {
         if (i % 2 == 1) {
             password[i] = rand() % 10 + '0';
@@ -41,9 +41,9 @@ int main(void)
         }
     }
 
-    password[11] = '\0'; // add null terminator
+    password[11] = '\0'; /* add null terminator */
 
-    printf("%s\n", password); // print the generated password
+    printf("%s\n", password); /* print the generated password */
 
     return 0;
 }
