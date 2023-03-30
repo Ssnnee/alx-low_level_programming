45f08a5518cb3b76dc1c99458bdf5aeb70666927
Samuel Nandi <nandisamuelsne@gmail.com>
2023-03-30 05:55:06 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer to destination string
+ * @src: pointer to source string
+ *
+ * Return: pointer to destination string
+ */
+char *_strcat(char *dest, const char *src)
+{
+	strcat(dest, src);
+	return dest;
+}
