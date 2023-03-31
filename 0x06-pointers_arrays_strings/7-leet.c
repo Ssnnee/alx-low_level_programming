af9fa634a67e20916564a787977b7f4d2a4f1f4b
Samuel Nandi <nandisamuelsne@gmail.com>
2023-03-30 20:02:29 -0700

    Function that encodes a string into 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,26 +2,26 @@
 
 /**
  * leet - encode into 1337speak
- * @str: input string to encode
+ * @n: input string to encode
  *
  * Return: pointer to the encoded string
  */
-char *leet(char *str)
+char *leet(char *n)
 {
 	int i, j;
 	const char s1[] = "aAeEoOtTlL";
 	const char s2[] = "4433007711";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; j < sizeof(s1); j++)
 		{
-			if (str[i] == s1[j])
+			if (n[i] == s1[j])
 			{
-				str[i] = s2[j];
+				n[i] = s2[j];
 				break;
 			}
 		}
 	}
-	return (str);
+	return (n);
 }
