29422099a30c5efd488f8c6a27a0a4f9a4b667d4
Samuel Nandi <nandisamuelsne@gmail.com>
2023-04-07 07:21:39 -0700

    Add __attribute__((unused)) to fix issues

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 /**
  * main - prints the name of the program
- * @argc: the number of arguments passed to the program
  * @argv: an array of pointers to the arguments
  *
  * Return: 0 if successful
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
