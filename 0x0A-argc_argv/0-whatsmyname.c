1c6dadea708c13005805c73d003929f419f1b450
Samuel Nandi <nandisamuelsne@gmail.com>
2023-04-10 15:51:44 -0700

    Add discription for argc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 /**
  * main - prints the name of the program
+ * @argc: the number of arguments passed to the program
  * @argv: an array of pointers to the arguments
  *
  * Return: 0 if successful
