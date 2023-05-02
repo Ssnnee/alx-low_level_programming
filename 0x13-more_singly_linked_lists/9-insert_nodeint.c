00d13c0e9b911d908882846b44c8add79e5c81d2
Samuel Nandi <nandisamuelsne@gmail.com>
2023-05-02 03:22:26 -0700

    Remove add_nodeint function and implement without it

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
 /**
  * insert_nodeint_at_index - Inserts a new node at a given position
  * @head: head of the list
@@ -17,17 +18,28 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (head == NULL)
 		return (NULL);
-	if (idx == 0)
-		return (add_nodeint(head, n));
-	current = *head;
-	for (i = 0; i < idx - 1 && current != NULL; i++)
-		current = current->next;
-	if (current == NULL)
-		return (NULL);
+
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	current = *head;
+	for (i = 0; i < idx - 1 && current != NULL; i++)
+		current = current->next;
+	if (current == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 	new_node->next = current->next;
 	current->next = new_node;
 	return (new_node);
