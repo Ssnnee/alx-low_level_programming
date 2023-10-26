172f4f3306ede1028ddbb2129b8cc34ba61b81ac
Ssnnee <nandisamuelsne@gmail.com>
2023-10-25 17:58:43 -0700

    Interview: not finished

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""In this module, I'am trying to solve a
+technical interview questions
+"""
+
+def island_perimeter(grid):
+    """The required func"""
+    if grid is type(list):
+        for c in grid:
+            if c is type(int):
+                pass
+    else:
+        print("Error")
+        quit(1)
+
+    if len(grid) < 100:
+        pass
+
